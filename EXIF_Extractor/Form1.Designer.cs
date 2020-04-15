0a1f8659b2711f7858731d046cabe0f9d36c7856
atime4ux <atime4ux@nate.com>
2020-04-16 08:35:03 +0900

    init commit

---
+++ EXIF_Extractor/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace EXIF_Extractor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sourcePath = new System.Windows.Forms.TextBox();
+            this.selSource = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // sourcePath
+            // 
+            this.sourcePath.Location = new System.Drawing.Point(18, 34);
+            this.sourcePath.Name = "sourcePath";
+            this.sourcePath.ReadOnly = true;
+            this.sourcePath.Size = new System.Drawing.Size(485, 21);
+            this.sourcePath.TabIndex = 0;
+            // 
+            // selSource
+            // 
+            this.selSource.Location = new System.Drawing.Point(517, 34);
+            this.selSource.Name = "selSource";
+            this.selSource.Size = new System.Drawing.Size(75, 20);
+            this.selSource.TabIndex = 1;
+            this.selSource.Text = "검색";
+            this.selSource.UseVisualStyleBackColor = true;
+            this.selSource.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(18, 176);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 20);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "실행";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "원본 경로";
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(99, 180);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(39, 12);
+            this.status.TabIndex = 5;
+            this.status.Text = "status";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 62);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(573, 108);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 208);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.selSource);
+            this.Controls.Add(this.sourcePath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox sourcePath;
+        private System.Windows.Forms.Button selSource;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ EXIF_Extractor/Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EXIF_Extractor
+{
+    public partial class Form2 : Form
+    {
+        DataTable DT;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public Form2(DataTable dt)
+        {
+            InitializeComponent();
+            this.DT = dt;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.DataSource = this.DT;
+        }
+    }
+}
